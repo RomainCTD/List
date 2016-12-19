8d3f2ded965a4ce5f2049148e00ed4819c61e81d
RomainCTD <chartaud.romain@gmail.com>
2016-12-19 19:21:36 +0100

    V7blink

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,6 @@
 class Music < ActiveRecord::Base
 	def self.search(search)
-  		where("genre ILIKE ? OR artiste ILIKE ? OR titre ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+  		where("genre ILIKE ? OR artiste ILIKE ?", "%#{search}%", "%#{search}%") 
 	end
 end
 
